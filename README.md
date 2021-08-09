8bdb60095c7d3aed717cef90d90dd169e9b2af4b
eduard93 <lebeduk93@gmail.com>
2021-08-09 17:58:02 +0300

    Create S3 connection once per session. Do not use intermediate files. Use log4j for logging. Removed wildcard imports.

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # HL7toS3Gateway
 
-HL7 to S3 gateway is a lihtweight process, allowing to capture HL7v2 messages sent 
-over TCP/MLLP protocol and upload them to the S3 bucket. These messages can be then processed by
-HealthShare Message Transformation Service.
+HL7 to S3 gateway is a lihtweight process, allowing to capture HL7v2 messages sent over TCP/MLLP protocol and upload them to the S3 bucket. These messages can be then processed by HealthShare Message Transformation Service.
 
-HL7toS3Gateway envisioned to be deployed within secure network of cloud VPC of the organization, so unencrypted HL7 TCP traffic never exposed to the public internet.
+HL7toS3Gateway envisioned to be deployed within secure network of cloud VPC of the organization, so unencrypted HL7 TCP traffic is never exposed to the public internet.
 
 ## Build
 ```
@@ -17,15 +15,36 @@ export AWS_ACCESS_KEY_ID=AKIA4ESCYD67GWODXXXX
 export AWS_SECRET_ACCESS_KEY='Ac/JNZd2uslCgLcEsy9HAVYkOQLW1Vsi2veXXXXX'
 export S3_BUCKET=anton-demo-bucket
 
-java -cp target/hl7tos3-1.0-SNAPSHOT-jar-with-dependencies.jar com.antonum.hl7tos3.MLLPBasedHL7ThreadedServer
+java -cp target/hl7tos3-1.0-SNAPSHOT-jar-with-dependencies.jar com.antonum.hl7tos3.MLLPBasedHL7ThreadedServer <port>
+```
+
+where `<port>` is a port for the server to run. Defaults to 1080.
+
+To set environment variables on Windows (PowerShell) use:
+
+```
+$env:AWS_ACCESS_KEY_ID = "AKIA4ESCYD67GWODXXXX"
+$env:AWS_SECRET_ACCESS_KEY = "Ac/JNZd2uslCgLcEsy9HAVYkOQLW1Vsi2veXXXXX"
+$env:S3_BUCKET = "anton-demo-bucket"
 ```
 
 ## Test
+
+Works on Mac, Linux and Windows (via WSL2):
 ```
 echo -n -e "\x0b$(cat hl7-file.txt )\x1c\x0d" | netcat localhost 1080
 ```
 
-GUI tool like HL7 Inspector - send messages to port 1080
+GUI tool like HL7 Inspector - send messages to port 1080.
+
+## Logging
+
+Logging is done via log4j version 2.14.
+Logging configuration is available in `src\main\resources\log4j2.xml`.
+By default log level is set to `DEBUG` with logging to both a file `all.log` and a console window.
+To change the logging configuration edit `src\main\resources\log4j2.xml` and recompile the jar file.
+
+
 ## References
 
 HL7 listener implementation is based on:
--- src/main/java/com/antonum/hl7tos3/MLLPBasedHL7ThreadedServer.java
+++ src/main/java/com/antonum/hl7tos3/MLLPBasedHL7ThreadedServer.java
@@ -1,265 +1,265 @@
 package com.antonum.hl7tos3;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
-	
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.BindException;
+import java.net.SocketException;
+import java.util.LinkedList;
+import java.util.UUID;
+import java.util.StringTokenizer;
+
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 
-import static java.nio.charset.StandardCharsets.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.time.Duration;
-import java.util.*;
-import java.util.concurrent.*;
+public class MLLPBasedHL7ThreadedServer {
 
-//import org.apache.commons.cli.*;
+    private int maxConnections;
+    private int listenPort;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+    // S3 client
+    final static private AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
 
-	public class MLLPBasedHL7ThreadedServer {
-		
-		private int maxConnections;
-		private int listenPort;
-		
-		//private static String connectionString = "{connection string}";
-		//private static String queueName = "{queue name}";
-		//private static final String queueName = "adtfromepic";
-		//private static IQueueClient queueClient;
+    // S3 bucket
+    final static private String s3_bucket = System.getenv("S3_BUCKET");
+    private static final Logger logger = LogManager.getLogger(MLLPBasedHL7ThreadedServer.class);
 
-	
-		
-		public MLLPBasedHL7ThreadedServer(int aListenPort, int maxConnections) {
-	        listenPort = aListenPort;
-	        this.maxConnections = maxConnections;
-	     }
-	     
-          public void acceptConnections() {
-	        try {
-	            ServerSocket server = new ServerSocket(listenPort, 5);
-	            Socket clientSocket = null;
-	            while (true) {
-	                clientSocket = server.accept();
-	                handleConnection(clientSocket);
-	            }
-	        } catch (BindException e) {
-	            System.out.println("Unable to bind to port " + listenPort);
-	        } catch (IOException e) {
-	            System.out.println("Unable to instantiate a ServerSocket on port: " + listenPort);
-	        }
-	     }
-	    
-	     protected void handleConnection(Socket connectionToHandle) {
-	        ConnectionHandler.processRequest(connectionToHandle);
-	     }
-	    
-	     public static void main(String[] args) {
 
-	        System.out.println("Starting MLLP server on port 1080");
-			MLLPBasedHL7ThreadedServer server = new MLLPBasedHL7ThreadedServer(1080, 3);
-	        server.setUpConnectionHandlers();
-	        server.acceptConnections();
-	     }
-	     public static void uploadFileToS3(String file_path, String bucket_name, String key_name) {
-			System.out.format("Uploading %s to S3 bucket %s...\n", key_name, bucket_name);
-			final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
-			try {
-				s3.putObject(bucket_name, key_name, new File(file_path));
-			} catch (AmazonServiceException e) {
-				System.err.println(e.getErrorMessage());
-				System.exit(1);
-			}
-		 }
-	     public void setUpConnectionHandlers() {
-	        for (int i = 0; i < maxConnections; i++) {
-	            ConnectionHandler currentHandler =
-	              new ConnectionHandler();
-	            Thread handlerThread = new Thread(currentHandler, "Handler " + i);
-	            handlerThread.setDaemon(true);
-	            handlerThread.start();
-	        }
-	     }
+    public MLLPBasedHL7ThreadedServer(int listenPort, int maxConnections) {
+        this.listenPort = listenPort;
+        this.maxConnections = maxConnections;
+    }
 
-	     private static class ConnectionHandler implements Runnable {
-	          private static final int MESSAGE_CONTROL_ID_LOCATION = 9;
-			private static final String FIELD_DELIMITER = "|";
-			private Socket connection;
-	          private static List pool = new LinkedList();
-	          static final char END_OF_BLOCK = '\u001c'; 
-	    	     static final char START_OF_BLOCK = '\u000b';
-	    	     static final char CARRIAGE_RETURN = 13; 
-	    	     private static final int END_OF_TRANSMISSION = -1;
-	        
-	          public ConnectionHandler() {
-	          }
-	        
-               public void handleConnection() {
-	        	 try {
-	            	System.out.println("Handling client at " 
-	            	+ connection.getInetAddress().getHostAddress() 
-	            	+ " on port " + connection.getPort());
-	
-	            	InputStream in = connection.getInputStream();
-	            	OutputStream out = connection.getOutputStream();
-	
-	            	String parsedHL7Message = getMessage(in);
-					System.out.println(parsedHL7Message);
-					writeHL7File(parsedHL7Message);
-					UUID uuid = UUID.randomUUID();
-					String key_name=uuid.toString()+".hl7";
-					uploadFileToS3("filename.txt",System.getenv("S3_BUCKET"),key_name);
-					//sendMessageToQueue(parsedHL7Message);
-					//System.out.println(parsedHL7Message);
-	            	String buildAcknowledgmentMessage = getSimpleAcknowledgementMessage(parsedHL7Message);
-	            	out.write(buildAcknowledgmentMessage.getBytes(), 0, buildAcknowledgmentMessage.length());
+    public static void main(String[] args) {
+        // Determine port to run. Defaults to 1080
+        String listenPortArg = "1080";
+        try {
+            listenPortArg = args[0];
+        } catch (ArrayIndexOutOfBoundsException e){
+            listenPortArg = "1080";
+        }
 
-	        	 } catch (IOException e) {
-				String errorMessage = "Error whiling reading and writing to connection " + e.getMessage();
-				System.out.print(errorMessage);
-				throw new RuntimeException(errorMessage);
-			 }	      
-	            
-	           finally {
-	              try {
-				  connection.close();
-				} 
-	              catch (IOException e) {
-				  String errorMessage = "Error whiling attempting to close to connection " + e.getMessage();
-				  System.out.println(errorMessage);
-				  throw new RuntimeException(errorMessage);
-	            	}	
-	            }
-	        }
-	        public static void writeHL7File(String message) {
-				try {
-					FileWriter myWriter = new FileWriter("filename.txt");
-					myWriter.write(message);
-					myWriter.close();
-					//System.out.println("Successfully wrote to the file.");
-				  } catch (IOException e) {
-					System.out.println("An error occurred.");
-					e.printStackTrace();
-				  }
-				
-			}
-			
-			public static void processRequest(Socket requestToHandle) {
-	            synchronized (pool) {
-	                pool.add(pool.size(), requestToHandle);
-	                pool.notifyAll();
-	             }
-	           }
+        int listenPort;
+        try {
+            listenPort = Integer.parseInt(listenPortArg);
+        } catch (NumberFormatException e) {
+            logger.warn("Supplied invalid port number: {}. Using 1080 instead.", listenPortArg);
+            listenPort = 1080;
+        }
 
-	           public void run() {
-	             while (true) {
-	                synchronized (pool) {
-	                    while (pool.isEmpty()) {
-	                        try {
-	                            pool.wait();
-	                        } catch (InterruptedException e) {
-	                            return;
-	                        }
-	                    }
-	                    connection = (Socket) pool.remove(0);
-	                }
-	                handleConnection();
-	             }
-	           }
-	        
-	          public String getMessage(InputStream anInputStream) throws IOException  {
-	       	 	        	
-	    			boolean end_of_message = false;
-				StringBuffer parsedMessage = new StringBuffer();
-	
-				int characterReceived = 0;
-	
-				try {
-					characterReceived = anInputStream.read();
-				} catch (SocketException e) {
-					System.out
-					.println("Unable to read from socket stream. "
-					+ "Connection may have been closed: " + e.getMessage());
-					return null;
-				}
-	
-				if (characterReceived == END_OF_TRANSMISSION) {
-					return null;
-				}
-	
-				if (characterReceived != START_OF_BLOCK) {
-					throw new RuntimeException(
-					"Start of block character has not been received");
-				}
-	
-				while (!end_of_message) {
-					characterReceived = anInputStream.read();
-						
-	
-					if (characterReceived == END_OF_TRANSMISSION) {
-						throw new RuntimeException(
-						"Message terminated without end of message character");
-					}
-	
-					if (characterReceived == END_OF_BLOCK) {
-						characterReceived = anInputStream.read();
-	
-						if (characterReceived != CARRIAGE_RETURN) {
-							throw new RuntimeException(
-							"End of message character must be followed by a carriage return character");
-						}
-						end_of_message = true;
-						//System.out.println();
-					} else {
-						parsedMessage.append((char) characterReceived);
-						//System.out.print(" "+(char)characterReceived);
-					}
-				}
-	
-				return parsedMessage.toString();
-	    	     }
-	    	   
-	    	    private String getSimpleAcknowledgementMessage(String aParsedHL7Message) {
-	    		 if (aParsedHL7Message == null)
-	    			throw new RuntimeException("Invalid HL7 message for parsing operation" +
-	    					". Please check your inputs");
-	    		   		
-	    		 String messageControlID = getMessageControlID(aParsedHL7Message);
-	    		
-	    		 StringBuffer ackMessage = new StringBuffer();
-	    		 ackMessage = ackMessage.append(START_OF_BLOCK)
-	    		                       .append("MSH|^~\\&|||||||ACK||P|2.2") 
-	    		                       .append(CARRIAGE_RETURN)
-	    		                       .append("MSA|AA|")
-	    		                       .append(messageControlID)
-	    		                       .append(CARRIAGE_RETURN)
-	    		                       .append(END_OF_BLOCK)
-	    		                       .append(CARRIAGE_RETURN);
-	    		
-	    		 return ackMessage.toString();
-	    	    }
-	    	
-		   private String getMessageControlID(String aParsedHL7Message) {
-			  int fieldCount = 0;
-			  StringTokenizer tokenizer = new StringTokenizer(aParsedHL7Message, FIELD_DELIMITER);
-				
-			  while (tokenizer.hasMoreElements())
-		        {
-		            String token = tokenizer.nextToken();
-		            fieldCount++;
-		            if (fieldCount == MESSAGE_CONTROL_ID_LOCATION){
-		            	return token;
-		            }		            
-		        }
-				
-				return "";	
-		   }
-	        
-	    }        
-	    
-	}
+        logger.info("Starting MLLP server on port {}", listenPort);
+        logger.info("S3 BUCKET: {}", System.getenv("S3_BUCKET"));
+
+        // Start TCP server
+        MLLPBasedHL7ThreadedServer server = new MLLPBasedHL7ThreadedServer(listenPort, 3);
+        server.setUpConnectionHandlers();
+        server.acceptConnections();
+    }
+
+    public static void uploadStringToS3(String content, String key_name) {
+        logger.info("Uploading {} to S3 bucket {}", key_name, s3_bucket);
+        try {
+            s3.putObject(s3_bucket, key_name, content);
+        } catch (AmazonServiceException e) {
+            logger.fatal("Error uploading to S3", e);
+            System.exit(1);
+        }
+        logger.info("Success!\n");
+    }
+
+
+
+    public void acceptConnections() {
+        try {
+            ServerSocket server = new ServerSocket(listenPort, 5);
+            Socket clientSocket = null;
+            while (true) {
+                clientSocket = server.accept();
+                handleConnection(clientSocket);
+            }
+        } catch (BindException e) {
+            logger.error("Unable to bind to port: {}", listenPort);
+        } catch (IOException e) {
+            logger.error("Unable to instantiate a ServerSocket on port: {} ", listenPort);
+        }
+    }
+
+    protected void handleConnection(Socket connectionToHandle) {
+        ConnectionHandler.processRequest(connectionToHandle);
+    }
+
+    public void setUpConnectionHandlers() {
+        for (int i = 0; i < maxConnections; i++) {
+            ConnectionHandler currentHandler =
+                    new ConnectionHandler();
+            Thread handlerThread = new Thread(currentHandler, "Handler " + i);
+            handlerThread.setDaemon(true);
+            handlerThread.start();
+        }
+    }
+
+    private static class ConnectionHandler implements Runnable {
+        static final char END_OF_BLOCK = '\u001c';
+        static final char START_OF_BLOCK = '\u000b';
+        static final char CARRIAGE_RETURN = 13;
+        private static final int MESSAGE_CONTROL_ID_LOCATION = 9;
+        private static final String FIELD_DELIMITER = "|";
+        private static final int END_OF_TRANSMISSION = -1;
+        private static LinkedList pool = new LinkedList();
+        private Socket connection;
+
+
+        public ConnectionHandler() {
+        }
+
+        public static void processRequest(Socket requestToHandle) {
+            synchronized (pool) {
+                pool.add(pool.size(), requestToHandle);
+                pool.notifyAll();
+            }
+        }
+
+        // Main method. Converts incoming stream into String and uploads it to S3
+        public void handleConnection() {
+            try {
+                logger.info("Handling client at {} on port {}", connection.getInetAddress().getHostAddress(), connection.getPort());
+
+                InputStream in = connection.getInputStream();
+                OutputStream out = connection.getOutputStream();
+
+                String parsedHL7Message = getMessage(in);
+                logger.trace(parsedHL7Message);
+
+                UUID uuid = UUID.randomUUID();
+                String key_name = uuid.toString() + ".hl7";
+                uploadStringToS3(parsedHL7Message, key_name);
+
+                String buildAcknowledgmentMessage = getSimpleAcknowledgementMessage(parsedHL7Message);
+                out.write(buildAcknowledgmentMessage.getBytes(), 0, buildAcknowledgmentMessage.length());
+
+            } catch (IOException e) {
+                String errorMessage = "Error while reading and writing to connection " + e.getMessage();
+                logger.error(errorMessage, e);
+                throw new RuntimeException(errorMessage);
+            } finally {
+                try {
+                    connection.close();
+                } catch (IOException e) {
+                    String errorMessage = "Error while attempting to close to connection " + e.getMessage();
+                    logger.error(errorMessage, e);
+                    throw new RuntimeException(errorMessage);
+                }
+            }
+        }
+
+        public void run() {
+            while (true) {
+                synchronized (pool) {
+                    while (pool.isEmpty()) {
+                        try {
+                            pool.wait();
+                        } catch (InterruptedException e) {
+                            return;
+                        }
+                    }
+                    connection = (Socket) pool.remove(0);
+                }
+                handleConnection();
+            }
+        }
+
+        public String getMessage(InputStream anInputStream) throws IOException {
+
+            boolean end_of_message = false;
+            StringBuffer parsedMessage = new StringBuffer();
+
+            int characterReceived = 0;
+
+            try {
+                characterReceived = anInputStream.read();
+            } catch (SocketException e) {
+                System.out
+                        .println("Unable to read from socket stream. "
+                                + "Connection may have been closed: " + e.getMessage());
+                return null;
+            }
+
+            if (characterReceived == END_OF_TRANSMISSION) {
+                return null;
+            }
+
+            if (characterReceived != START_OF_BLOCK) {
+                throw new RuntimeException(
+                        "Start of block character has not been received");
+            }
+
+            while (!end_of_message) {
+                characterReceived = anInputStream.read();
+
+
+                if (characterReceived == END_OF_TRANSMISSION) {
+                    throw new RuntimeException(
+                            "Message terminated without end of message character");
+                }
+
+                if (characterReceived == END_OF_BLOCK) {
+                    characterReceived = anInputStream.read();
+
+                    if (characterReceived != CARRIAGE_RETURN) {
+                        throw new RuntimeException(
+                                "End of message character must be followed by a carriage return character");
+                    }
+                    end_of_message = true;
+                } else {
+                    parsedMessage.append((char) characterReceived);
+                }
+            }
+
+            return parsedMessage.toString();
+        }
+
+        private String getSimpleAcknowledgementMessage(String aParsedHL7Message) {
+            if (aParsedHL7Message == null)
+                throw new RuntimeException("Invalid HL7 message for parsing operation" +
+                        ". Please check your inputs");
+
+            String messageControlID = getMessageControlID(aParsedHL7Message);
+
+            StringBuffer ackMessage = new StringBuffer();
+            ackMessage = ackMessage.append(START_OF_BLOCK)
+                    .append("MSH|^~\\&|||||||ACK||P|2.2")
+                    .append(CARRIAGE_RETURN)
+                    .append("MSA|AA|")
+                    .append(messageControlID)
+                    .append(CARRIAGE_RETURN)
+                    .append(END_OF_BLOCK)
+                    .append(CARRIAGE_RETURN);
+
+            return ackMessage.toString();
+        }
+
+        private String getMessageControlID(String aParsedHL7Message) {
+            int fieldCount = 0;
+            StringTokenizer tokenizer = new StringTokenizer(aParsedHL7Message, FIELD_DELIMITER);
+
+            while (tokenizer.hasMoreElements()) {
+                String token = tokenizer.nextToken();
+                fieldCount++;
+                if (fieldCount == MESSAGE_CONTROL_ID_LOCATION) {
+                    return token;
+                }
+            }
+
+            return "";
+        }
+
+    }
+
+}
